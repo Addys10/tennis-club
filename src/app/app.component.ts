88f761b44bc3003ddc18ef19c8b150ff61ae03f7
Adys <adamhanus10@gmail.com>
2024-11-21 23:26:39 +0100

    fix: formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from './core/services/auth.service';
-import { User } from './core/models/auth.model';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService} from '@core/services/auth.service';
+import {User} from '@core/models/auth.model';
 
 @Component({
   selector: 'app-root',
